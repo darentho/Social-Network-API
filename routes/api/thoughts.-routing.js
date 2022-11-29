2ea58c2cfee34154afa44c9a2f3d95334fc1c54b
Alberto Monroy <firewind3882@gmail.com>
2022-11-28 22:54:47 -0600

    started the thought controrller

--- routes/api/thoughts.-routing.js
+++ routes/api/thoughts.-routing.js
@@ -1,1 +1,18 @@
 const router = require("express").Router();
+
+const thoughtController = {
+  getAllThoughts({}, res) {
+    Thought.find()
+      .populate({
+        path: "reactions",
+        select: "-__v",
+      })
+      .select("-__v")
+      .sort({ _id: -1 })
+      .then((dbThoughtData) => res.json(dbThoughtData))
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+};
