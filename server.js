fd116736d866fb0160b94db7e0152012d1ea5d50
Alberto Monroy <firewind3882@gmail.com>
2022-11-28 21:27:53 -0600

    added the server and mongodb connection

---
+++ server.js
@@ -0,0 +1,27 @@
+//Requiring Modules
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+//express middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(require("./routes"));
+
+//establishing mongo connection
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/social-network-API",
+  {
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+// This will execute mongo queries.
+mongoose.set("debug", true);
+
+app.listen(PORT, () => console.log(`Listening on port:${PORT}`));
