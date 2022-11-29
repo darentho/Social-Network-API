77e40409df5ea2392ed39d0d84ee1798850b0a90
Alberto Monroy <firewind3882@gmail.com>
2022-11-29 00:08:30 -0600

    I finally got it to work... was having lots of trouble with the mongodb URL

--- server.js
+++ server.js
@@ -12,13 +12,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(require("./routes"));
 
 //establishing mongo connection
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/social-network-API",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
+mongoose.connect(process.env.MONGODB_URI || "mongodb://0.0.0.0:27017/", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 // This will execute mongo queries.
 mongoose.set("debug", true);
