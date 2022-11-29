bed3b5b8e72790da3e7e6f9b81268b4ca353f320
Alberto Monroy <firewind3882@gmail.com>
2022-11-28 22:22:12 -0600

    added the user Schema with the friends array

--- models/User.js
+++ models/User.js
@@ -1,23 +1,53 @@
 const { Schema, model } = require("mongoose");
 const moment = require("moment");
 
-const UserSchema = new Schema({
-  username: {
-    type: String,
-    unique: true,
-    required: true,
-    trim: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    match: [/.+@.+\..+/, "You must enter a valid email address"],
-  },
-  thoughts: [
-    {
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      //we're using an email validation as well as a prompt
+      match: [/.+@.+\..+/, "You must enter a valid email address"],
+    },
+    thoughts: [
+      {
         type: Schema.Types.ObjectId,
-        ref: "'
-    }
-  ]
+        ref: "Thought",
+      },
+    ],
+    //Here's where we're generating the friends array and referencing it to User.
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+UserSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
 });
+
+UserSchema.pre("remove", function (next) {
+  Thought.remove({ username: this.username }).exec();
+  next();
+});
+
+const User = model("User", UserSchema);
+
+module.exports = User;
